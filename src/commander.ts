2533f0aba8459de7b49fbcd8d252daa2a0c3af29
Klaysb <sclbrito@hotmail.com>
2020-02-26 20:01:48 +0000

    add container view with three commands

---
+++ src/commander.ts
@@ -0,0 +1,45 @@
+import * as vscode from 'vscode'
+
+export class GZoltarCommander implements vscode.TreeDataProvider<GZoltarCommand> {
+
+    commands: GZoltarCommand[] = []
+
+    constructor() {
+        this.buildCommander();
+    }
+
+    buildCommander() {
+        const runTestCommand = new GZoltarCommand('Run Test Methods', vscode.TreeItemCollapsibleState.None, {command: 'gzoltar.run', title: ''});
+        const reportCommand = new GZoltarCommand('Generate Report', vscode.TreeItemCollapsibleState.None, {command: 'gzoltar.report', title: ''});
+        const showviewCommand = new GZoltarCommand('Show View', vscode.TreeItemCollapsibleState.None, {command: 'gzoltar.show', title: ''});
+
+        this.commands.push(runTestCommand, reportCommand, showviewCommand);
+    }  
+
+    getTreeItem(element: GZoltarCommand): vscode.TreeItem | Thenable<vscode.TreeItem> {
+        return element;
+    }
+
+    getChildren(element?: GZoltarCommand | undefined): vscode.ProviderResult<GZoltarCommand[]> {
+        if (!element) {
+            return Promise.resolve(this.commands);
+        }
+
+        return Promise.resolve(element.children);
+    }
+
+
+}
+
+export class GZoltarCommand extends vscode.TreeItem {
+
+    public children: GZoltarCommand[] = []
+
+    constructor(
+        public readonly label: string, 
+        public readonly collapsibleState: vscode.TreeItemCollapsibleState,
+        public readonly command?: vscode.Command
+    ) {
+        super(label, collapsibleState)
+    }
+}
--- src/extension.ts
+++ src/extension.ts
@@ -1,27 +1,24 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+'use strict'
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
+import * as vscode from 'vscode';
+import { GZoltarCommander } from './commander';
+
 export function activate(context: vscode.ExtensionContext) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "vscode-gzoltar" is now active!');
+	const commander = new GZoltarCommander();
+	vscode.window.registerTreeDataProvider('gzoltar', commander);
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
+	vscode.commands.registerCommand('gzoltar.run', () => {
+		vscode.window.showInformationMessage('Run was activated.');
+	})
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World!');
-	});
+	vscode.commands.registerCommand('gzoltar.report', () => {
+		vscode.window.showInformationMessage('Report was activated.');
+	})
 
-	context.subscriptions.push(disposable);
+	vscode.commands.registerCommand('gzoltar.show', () => {
+		vscode.window.showInformationMessage('Show was activated.');
+	})
 }
 
-// this method is called when your extension is deactivated
 export function deactivate() {}

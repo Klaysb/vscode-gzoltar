9599e83d0f4e1197dd2b65a658f1ff5dfec6aeda
Klaysb <sclbrito@hotmail.com>
2020-03-06 15:50:13 +0000

    changes commands to use gzoltar cli

--- src/commander.ts
+++ src/commander.ts
@@ -9,12 +9,12 @@ export class GZoltarCommander implements vscode.TreeDataProvider<GZoltarCommand>
     }
 
     buildCommander() {
-        const cleanCommand = new GZoltarCommand('Clean Project', vscode.TreeItemCollapsibleState.None, {command: 'gzoltar.clean', title: ''});
+        const listCommand = new GZoltarCommand('List Test Methods', vscode.TreeItemCollapsibleState.None, {command: 'gzoltar.list', title: ''});
         const runTestCommand = new GZoltarCommand('Run Test Methods', vscode.TreeItemCollapsibleState.None, {command: 'gzoltar.run', title: ''});
         const reportCommand = new GZoltarCommand('Generate Report', vscode.TreeItemCollapsibleState.None, {command: 'gzoltar.report', title: ''});
         const showviewCommand = new GZoltarCommand('Show View', vscode.TreeItemCollapsibleState.None, {command: 'gzoltar.show', title: ''});
 
-        this.commands.push(cleanCommand, runTestCommand, reportCommand, showviewCommand);
+        this.commands.push(listCommand, runTestCommand, reportCommand, showviewCommand);
     }  
 
     getTreeItem(element: GZoltarCommand): vscode.TreeItem | Thenable<vscode.TreeItem> {
--- src/extension.ts
+++ src/extension.ts
@@ -3,52 +3,63 @@
 import * as vscode from 'vscode';
 import { exec } from 'child_process';
 import { GZoltarCommander } from './commander';
-import * as path from 'path';
+import { readFile } from 'fs';
 
 export function activate(context: vscode.ExtensionContext) {
 
 	const workspaceFolders = vscode.workspace.workspaceFolders;
 	const workspace = workspaceFolders != undefined? workspaceFolders[0].uri.fsPath : ''; //TODO
 
-	const mvn = path.join(__filename, '..', '..', 'tools', 'maven', 'apache-maven-3.6.3', 'bin');
-	const gz = path.join(__filename, '..', '..', 'tools', 'gzoltar');
-	const junit = path.join(__filename, '..', '..', 'tools', 'junit');
+	const toolsPath = `${context.extensionPath}/tools`
 	const commander = new GZoltarCommander();
 
 	vscode.window.registerTreeDataProvider('gzoltar', commander);
 
-	vscode.commands.registerCommand('gzoltar.clean', () => {
-		vscode.window.showInformationMessage('Clean was activated.');
-		
-		exec(`(cd ${workspace} && ${mvn}/mvn clean test-compile)`, (err, stdout, stderr) => {
+	vscode.commands.registerCommand('gzoltar.list', () => {
+		vscode.window.showInformationMessage('List was activated.');
+
+		exec(`(cd ${toolsPath} && java -javaagent:"gzoltaragent.jar" -cp "${workspace}":"junit-4.13.jar":"gzoltarcli.jar" com.gzoltar.cli.Main listTestMethods ${workspace})`, (err, stdout, stderr) => {
 			if(err) return vscode.window.showErrorMessage(err.message);
-			vscode.window.showInformationMessage('Clean completed.')
+			vscode.window.showInformationMessage('List completed.')
 		})
-	})
+	});
 
 	vscode.commands.registerCommand('gzoltar.run', () => {
 		vscode.window.showInformationMessage('Run was activated.');
 
-		//${mvn}/mvn -P sufire gzoltar:prepare-agent test
-		//cd ${workspace} &&
-		exec(`(java -cp ${workspace};${junit}/junit-4.13.jar;${gz}/gzoltarcli.jar com.gzoltar.cli.Main listTestMethods ${workspace}/target/test-classes/org/gzoltar/examples --outputFile ${workspace}/target/a.txt)`, (err, stdout, stderr) => {
+		exec(`(cd ${toolsPath} && java -javaagent:"gzoltaragent.jar" -cp "${workspace}":"junit-4.13.jar":"gzoltarcli.jar" com.gzoltar.cli.Main runTestMethods --testMethods tests.txt --collectCoverage)`, (err, stdout, stderr) => {
 			if(err) return vscode.window.showErrorMessage(err.message);
 			vscode.window.showInformationMessage('Run completed.')
-		})
-	})
+		});
+	});
 
 	vscode.commands.registerCommand('gzoltar.report', () => {
 		vscode.window.showInformationMessage('Report was activated.');
 
-		exec(`(cd ${workspace} && ${mvn}/mvn gzoltar:fl-report)`, (err, stdout, stderr) => {
+		exec(`(cd ${toolsPath} && java -javaagent:"gzoltaragent.jar" -cp "${workspace}":"junit-4.13.jar":"gzoltarcli.jar" com.gzoltar.cli.Main faultLocalizationReport --buildLocation ${workspace} --dataFile gzoltar.ser --outputDirectory ${toolsPath} --formatter HTML)`, (err, stdout, stderr) => {
 			if(err) return vscode.window.showErrorMessage(err.message);
 			vscode.window.showInformationMessage('Report completed.')
-		})
-	})
+		});
+	});
 
 	vscode.commands.registerCommand('gzoltar.show', () => {
 		vscode.window.showInformationMessage('Show was activated.');
-	})
+
+		readFile(`${toolsPath}/sfl/html/ochiai/sunburst.html`, (err, data) => {
+			if (err) throw err;
+			const html = data.toString();
+			const webview = vscode.window.createWebviewPanel(
+				'report',
+				'Report',
+				vscode.ViewColumn.Beside,
+				{
+					enableScripts: true,
+					localResourceRoots: [vscode.Uri.file(toolsPath)]
+				}
+			);
+			webview.webview.html = html;
+		  });
+	});
 }
 
 export function deactivate() {}

96edf1bd00feb6191626cb3ebf0fd03267c9e2f0
Klaysb <sclbrito@hotmail.com>
2020-05-19 13:11:12 +0100

    change html and folder structure

--- src/reportPanel.ts
+++ src/reportPanel.ts
@@ -75,39 +75,41 @@ export class ReportPanel {
 
     private formatMain(views: string[]): string {
         return `<html>
-                    <head>
-                        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
-                        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-                        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-                        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
-                    </head>
-                    <body style="background: rgba(0,0,0,0);">
-                        <ul class="nav nav-tabs" id="myTab" role="tablist">
-                            <li class="nav-item">
-                                <a class="nav-link active" id="sunburst-tab" data-toggle="tab" href="#sunburst" role="tab" aria-controls="sunburst" aria-selected="true">Sunburst</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" id="bubble-tab" data-toggle="tab" href="#bubble" role="tab" aria-controls="bubble" aria-selected="false">Bubble Hierarchy</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" id="vertical-tab" data-toggle="tab" href="#vertical" role="tab" aria-controls="vertical" aria-selected="false">Vertical Partition</a>
-                            </li>
-                        </ul>
-                        <div class="tab-content" id="myTabContent">
-                            <div class="tab-pane fade show active" id="sunburst" role="tabpanel" aria-labelledby="sunburst-tab">
-                                <iframe src="${views[0]}" style="overflow: hidden;border:none;" scrolling='no' height='100%' width='100%' allowfullscreen></iframe>
-                            </div>
-                
-                            <div class="tab-pane fade" id="bubble" role="tabpanel" aria-labelledby="bubble-tab">
-                                <iframe src="${views[1]}" style="overflow: hidden;border:none;" scrolling='no' height='100%' width='100%' allowfullscreen></iframe>
-                            </div>
-                
-                            <div class="tab-pane fade" id="vertical" role="tabpanel" aria-labelledby="vertical-tab">
-                                <iframe src="${views[2]}" style="overflow: hidden;border:none;" scrolling='no' height='100%' width='100%' allowfullscreen></iframe>
-                            </div>
-                        </div>
-                    </body>
-                </html>`;
+        <head>
+            <title>GZoltar</title>
+        </head>
+
+        <body>
+            <div>
+                <ul>
+                    <button onclick="change('sunburst')">Sunburst</button>
+                    <button onclick="change('bubble')">Bubble Hierarchy</button>
+                    <button onclick="change('vertical')">Vertical Partition</button>
+                </ul>
+            </div>
+            <div id="sunburst">
+                <iframe src="${views[0]}" id="sunburstF" style="overflow: hidden;border:none;" scrolling='no' height='100%'
+                    width='100%' allowfullscreen></iframe>
+            </div>
+            <div id="bubble" style="display: none;">
+                <iframe src="${views[1]}" id="bubbleF" style="overflow: hidden;border:none;" scrolling='no'
+                        height='100%' width='100%' allowfullscreen></iframe>
+            </div>
+            <div id="vertical" style="display: none;">
+                <iframe src="${views[2]}" id="verticalF" style="overflow: hidden;border:none;" scrolling='no'
+                    height='100%' width='100%' allowfullscreen></iframe>
+            </div>
+            <script>
+                let active = document.getElementById('sunburst');
+    
+                function change(id) {
+                    active.style.display = 'none';
+                    active = document.getElementById(id);
+                    active.style.display = 'inline';
+                }
+            </script>
+        </body>
+    </html>`;
     }
 }
 
